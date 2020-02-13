d0189389770b0de4ddebdbd60c68a3e7f365951d
Andreas Timoudas <andreas@Andreass-Air.lan>
2020-02-13 15:08:27 +0100

    updaterat kod, ser till att den sparar json filer i json mappen och att anrop mot apin funkar

--- API_scraper/model/api_scraper.py
+++ API_scraper/model/api_scraper.py
@@ -1,181 +1,276 @@
 import requests
 import json
 from pprint import pprint
-from tqdm import tqdm
+import os.path
 
-class Premier_league:
-    
-    def __init__(self):
-        self.base_url = 'https://footballapi.pulselive.com/football'
-    
-    def get_competion_id(self):
-        competitions = {} #Store all competitions
-        league = {} #Store info for each competion
-        url = self.base_url + '/competitions' 
-        params = (
-            ('pageSize', '100'),#adds ?pageSize=100 to url
-        )
-        response = requests.get(url, params = params).json() # request to obtain the id values and corresponding competition
 
-        all_comps = response["content"]
-        
-        #loop to get all info for all competitions
-        for comp in all_comps: 
-            
-            league[comp["id"]] = comp["description"]
+class PremierLeague:
+	
+	def __init__(self, base_url='https://footballapi.pulselive.com/football'):
+		self.base_url = base_url
+		self.dirname = os.path.dirname(__file__)
+		self.headers = {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+						'Origin': 'https://www.premierleague.com',
+						'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'
+						}
 
-          # creating a stat dict for the player
-            competitions[league[comp["id"]]] = {"info":{}}
-            competitions[league[comp["id"]]]["info"]["abbreviation"] = comp["abbreviation"]
-            competitions[league[comp["id"]]]['info']['id'] = comp['id'] 
-            
-        f = open("../json/competitions.json","w")
+	
+	def save_json(self,file, filename):
+		file = os.path.join(self.dirname, "../json/" + str(file) + ".json" )
+		with open(file, "w") as f:
+			#pretty prints and writes the same to the json file 
+			f.write(json.dumps(filename, indent=4, sort_keys=False))
+		
+	def get_competion_id(self):
 
-        # pretty prints and writes the same to the json file 
-        f.write(json.dumps(competitions,indent=4, sort_keys=False))
-        f.close()
-              
-    def get_clubs(self):
-        clubs = {} #Store all clubs
-        team = {} #Store info for each team
-        url = self.base_url + '/clubs' 
-        
-        page = 0 #starting value of page
-        while True:
-            params = (
-                ('pageSize', '100'),
-                ('page', str(page))#adds ?pageSize=100 to url
-                    )
-            response = requests.get(url, params = params).json() # request to obtain the team info
+		url = self.base_url + '/competitions' 
 
-            all_clubs = response["content"]
-            
-            
-                    #loop to get all info for all competitions
-            for club in all_clubs: 
-                clubs[club['name']]= club['teams'][0]['id']
-                
-                #Unessesary code below, might be of use, produces complex dict-structure
-                #team[club["id"]] = club["name"]
-                #clubs[team[club["id"]]] = {"info":{}}
-                #clubs[team[club["id"]]]['info']['name'] = club["name"]
-                #clubs[team[club["id"]]]['info']["id"]= club['teams'][0]['id']
+		params = (('pageSize', '100'),)#adds ?pageSize=100 to url
+			
+		response = requests.get(url, params = params, headers=self.headers).json() # request to obtain the id values and corresponding competition
 
-            page += 1
-            if page == response["pageInfo"]["numPages"]:
-                break
-            
-        f = open("../json/clubs.json","w")
+		all_comps = response["content"]
+		
+		competitions = {}
+		
+		for comp in all_comps:   
 
-        # pretty prints and writes the same to the json file 
-        f.write(json.dumps(clubs,indent=4, sort_keys=False))
-        f.close()
+			competition_id = comp['description']
+			index = competition_id
+			competitions[index] = \
+				 {
+				 'abbreviation': comp["abbreviation"],
+				  'id' : comp['id'],
+				 }
+		
+		self.save_json("competitions", competitions)
 
-    
-    def get_fixtures(self,compSeasons):
+	def get_clubs(self):
+		
+		clubs = {} #Store all clubs
+		url = self.base_url + '/clubs' 
+		
+		page = 0 #starting value of page
+		while True:
+			params = (
+				('pageSize', '100'),
+				('page', str(page))#adds ?pageSize=100 to url
+					)
+			response = requests.get(url, params = params, headers=self.headers).json() # request to obtain the team info
 
-        fixtures_unplayed = {} #Store info for not played fixtures
-        games_unplayed = {} #Store info for not played games
-        
-        fixtures_played = {} #Store all clubs
-        games_played = {} #Store info for each team
-        url = self.base_url + '/fixtures' 
+			all_clubs = response["content"]
+						
+
+			#loop to get all info for all competitions
+			for club in all_clubs: 
+				print(club['name'])
+				print(club['teams'][0]['id'])
+				clubs[club['name']] = club['teams'][0]['id']
+
+			page += 1
+			if page == response["pageInfo"]["numPages"]:
+				break
+			
+		self.save_json("clubs", clubs)
+
+	def get_fixtures(self,compSeasons):
+
+		url = self.base_url + '/fixtures' 
+
+		fixtures_unplayed = {} #Store info for not played fixtures
+			
+		
+		fixtures_played = {} #Store all clubs
 
    
-        page = 0 #starting value of page
-        while True:
-            params = (
-                ('pageSize', '100'), #adds ?pageSize=100 to url
-                ('page', str(page)),
-                ('compSeasons', str(compSeasons)),
-                    )
-            response = requests.get(url, params = params).json() # request to obtain the team info
+		page = 0 #starting value of page
+		while True:
+			params = (
+				('pageSize', '100'), #adds ?pageSize=100 to url
+				('page', str(page)),
+				('compSeasons', str(compSeasons)),
+					)
+			response = requests.get(url, params = params, headers=self.headers).json() # request to obtain the team info
 
-            all_games = response["content"]
-            
-            #loop to get info for each game 
-            
-            for game in tqdm(all_games): 
-                if game['status'] == 'U':
-                    games_unplayed[game["id"]] = game['id']
-                    fixtures_unplayed[games_unplayed[game["id"]]] = {"match":{}}
-                    fixtures_unplayed[games_unplayed[game["id"]]]['match'] = game['id']
-                    fixtures_unplayed[games_unplayed[game["id"]]]['kickoff'] = game['fixtureType']
-                    fixtures_unplayed[games_unplayed[game["id"]]]['preli_date'] = game['provisionalKickoff']['label']
-                    fixtures_unplayed[games_unplayed[game["id"]]]['scientific_date'] = game['provisionalKickoff']['millis']
-                    fixtures_unplayed[games_unplayed[game["id"]]]['home_team'] = game['teams'][0]['team']['name']
-                    fixtures_unplayed[games_unplayed[game["id"]]]['home_team_id'] = game['teams'][0]['team']['club']['id']
-                    fixtures_unplayed[games_unplayed[game["id"]]]['home_team_abbr'] = game['teams'][0]['team']['club']['abbr']
-                    fixtures_unplayed[games_unplayed[game["id"]]]['away_team'] = game['teams'][1]['team']['name']
-                    fixtures_unplayed[games_unplayed[game["id"]]]['away_team_id'] = game['teams'][1]['team']['club']['id']
-                    fixtures_unplayed[games_unplayed[game["id"]]]['away_team_abbr'] = game['teams'][1]['team']['club']['abbr']
-                    fixtures_unplayed[games_unplayed[game["id"]]]['grounds'] = game['ground']['name']
-                    fixtures_unplayed[games_unplayed[game["id"]]]['grounds_id'] = game['ground']['id']
-                    fixtures_unplayed[games_unplayed[game["id"]]]['gameweek'] = game['gameweek']['gameweek']
-                    fixtures_unplayed[games_unplayed[game["id"]]]['status'] = game['status']
+			all_games = response["content"]
+			
+			#loop to get info for each game 
+			for game in all_games: 
 
-            for game in tqdm(all_games): 
+				if game['status'] == 'U':
+					
 
-                if game['status'] == 'C':
+					game_id = game['id']
+					index = game_id
+					fixtures_unplayed[index] = \
+						{
+						'match' : game_id,
+						'kickoff' : game['fixtureType'],
+						'preli_date' : game['provisionalKickoff']['label'],
+						'scientific_date' : game['provisionalKickoff']['millis'],
+						'home_team' : game['teams'][0]['team']['name'],
+						'home_team_id' : game['teams'][0]['team']['club']['id'],
+						'home_team_abbr' : game['teams'][0]['team']['club']['abbr'],
+						'away_team' : game['teams'][1]['team']['name'],
+						'away_team_id' : game['teams'][1]['team']['club']['id'],
+						'away_team_abbr' : game['teams'][1]['team']['club']['abbr'],
+						'grounds' : game['ground']['name'],
+						'grounds_id' : game['ground']['id'],
+						'gameweek' : game['gameweek']['gameweek'],
+						'status' : game['status'],
+						}
 
-                    games_played[game["id"]] = game['id']
-                    fixtures_played[games_played[game["id"]]] = {"match":{}}
-                    fixtures_played[games_played[game["id"]]]['match'] = game['id']
-                    fixtures_played[games_played[game["id"]]]['kickoff'] = game['fixtureType']
-                    fixtures_played[games_played[game["id"]]]['preli_date'] = game['provisionalKickoff']['label']
-                    fixtures_played[games_played[game["id"]]]['scientific_date'] = game['provisionalKickoff']['millis']
-                    fixtures_played[games_played[game["id"]]]['home_team'] = game['teams'][0]['team']['name']
-                    fixtures_played[games_played[game["id"]]]['home_team_id'] = game['teams'][0]['team']['club']['id']
-                    fixtures_played[games_played[game["id"]]]['home_team_abbr'] = game['teams'][0]['team']['club']['abbr']
-                    fixtures_played[games_played[game["id"]]]['home_team_score'] = game['teams'][0]['score']
-                    fixtures_played[games_played[game["id"]]]['away_team'] = game['teams'][1]['team']['name']
-                    fixtures_played[games_played[game["id"]]]['away_team_id'] = game['teams'][1]['team']['club']['id']
-                    fixtures_played[games_played[game["id"]]]['away_team_abbr'] = game['teams'][1]['team']['club']['abbr']
-                    fixtures_played[games_played[game["id"]]]['away_team_score'] = game['teams'][1]['score']
-                    fixtures_played[games_played[game["id"]]]['grounds'] = game['ground']['name']
-                    fixtures_played[games_played[game["id"]]]['grounds_id'] = game['ground']['id']
-                    fixtures_played[games_played[game["id"]]]['gameweek'] = game['gameweek']['gameweek']
-                    fixtures_played[games_played[game["id"]]]['outcome'] = game['outcome']
-                    fixtures_played[games_played[game["id"]]]['extraTime'] = game['extraTime']
-                    fixtures_played[games_played[game["id"]]]['shootout'] = game['shootout']
-                    fixtures_played[games_played[game["id"]]]['played_time'] = game['clock']['secs']
-                    fixtures_played[games_played[game["id"]]]['played_time_label'] = game['clock']['label']
-                    fixtures_played[games_played[game["id"]]]['status'] = game['status'] 
-   
-                     
 
-                
-            page +=1
-            if page == response["pageInfo"]["numPages"]:
-                break
-        
-        fixtures = dict(fixtures_unplayed)
-        fixtures.update(fixtures_played)
+			for game in all_games: 
 
-        with open("../json/unplayed_fixtures.json","w") as f:
-            f.write(json.dumps(fixtures_unplayed,indent=4, sort_keys=True))
+				if game['status'] == 'C':
+					
+					game_id = game['id']
+					index = game_id
+					fixtures_played[index] = \
+					{
+					'match' : game['id'],
+					'kickoff' : game['fixtureType'],
+					'preli_date' : game['provisionalKickoff']['label'],
+					'scientific_date' : game['provisionalKickoff']['millis'],
+					'home_team' : game['teams'][0]['team']['name'],
+					'home_team_id' : game['teams'][0]['team']['club']['id'],
+					'home_team_abbr' : game['teams'][0]['team']['club']['abbr'],
+					'home_team_score' : game['teams'][0]['score'],
+					'away_team' : game['teams'][1]['team']['name'],
+					'away_team_id' : game['teams'][1]['team']['club']['id'],
+					'away_team_abbr' : game['teams'][1]['team']['club']['abbr'],
+					'away_team_score' : game['teams'][1]['score'],
+					'grounds' : game['ground']['name'],
+					'grounds_id' : game['ground']['id'],
+					'gameweek' : game['gameweek']['gameweek'],
+					'outcome' : game['outcome'],
+					'extraTime'  : game['extraTime'],
+					'shootout' : game['shootout'],
+					'played_time' : game['clock']['secs'],
+					'played_time_label' : game['clock']['label'],
+					'status' : game['status'],
+					'goals' : game['goals'],
+					}
+				   
+			page +=1
+			if page == response["pageInfo"]["numPages"]:
+				break
+		
+		fixtures = dict(fixtures_unplayed)
+		fixtures.update(fixtures_played)
 
-        with open("../json/played_fixtures.json","w") as f:
-            f.write(json.dumps(fixtures_played,indent=4, sort_keys=True))
+		self.save_json("fixtures_unplayed", fixtures_unplayed)
+		self.save_json("fixtures_played", fixtures_played)
+		self.save_json("fixtures", fixtures)
 
-        with open("../json/fixtures.json","w") as f:
-            f.write(json.dumps(fixtures,indent=4, sort_keys=True))
+	def get_standings(self, compSeasons):
 
-        
+		url = self.base_url + '/compseasons/{}/standings'.format(compSeasons)
+		
+		
+		params = (
+			('pageSize', '100'),
+				)
+		response = requests.get(url, params = params, headers=self.headers).json() # request to obtain the team info
 
-         
+		all_standings = response["tables"][0]['entries']
+		season_id = response['compSeason']['id']
+
+		
+		
+		standings = {} #Store all standings
+
+
+		#loop to get all info for all standings
+		for standing in all_standings:
+			standing_id = standing['team']['name']
+			index = standing_id
+
+			standings[index] = \
+			{
+			'season_id' : season_id,
+			'team_id' : standing['team']['club']['id'],
+			'position' : standing['position'],
+			'overall' : standing['overall'],
+			'home' : standing['home'],
+			'away' : standing['away'],
+			}
+
+
+		self.save_json("standings", standings)
+
+	def get_team_standing(self, compSeasons, teamId):
+
+		url = self.base_url + '/compseasons/{}/standings/team/{}'.format(compSeasons,teamId)
+		
+		params = (
+			('pageSize', '100'),
+				)
+		response = requests.get(url, params = params, headers=self.headers).json() # request to obtain the team info
+
+
+		team_standing = response['entries'] #team standing through season
+		season_id = response['compSeason']['label'] #season label
+		team = response['team'] #team name
+		team_name = str(response['team']['name'])
+		
+		team_standings = {} #Store all standings
+
+
+		#loop to get all info for all standings
+		for standing in team_standing:
+			standing_id = standing['played']
+			index = standing_id
+
+			team_standings[index] = \
+			{
+			'season' : season_id,
+			'season_id' : standing['fixtures'][0]['gameweek']['compSeason']['id'],
+			'team_id' : team['club']['id'],
+			'team' : team['name'],
+			'position' : standing['position'],
+			'points' : standing['points'],
+			'played_games' : standing['played'],
+			'game_week_id' : standing['fixtures'][0]['gameweek']['id'],
+			'game_week': standing['fixtures'][0]['gameweek']['gameweek'],
+			'competition' : standing['fixtures'][0]['gameweek']['compSeason']['competition']['description'],
+			'game_id' : standing['fixtures'][0]['id'],
+			'home_team' : standing['fixtures'][0]['teams'][0]['team']['name'],
+			'home_team_id' : standing['fixtures'][0]['teams'][0]['team']['club']['id'],
+			'home_team_score': standing['fixtures'][0]['teams'][0]['score'],
+			'away_team' : standing['fixtures'][0]['teams'][1]['team']['name'],
+			'away_team_id' : standing['fixtures'][0]['teams'][1]['team']['club']['id'],
+			'away_team_score': standing['fixtures'][0]['teams'][1]['score'] 
+			}
+
+
+		f = open(team_name + "_standings_" + str(compSeasons) + ".json", "w")
+
+		# pretty prints and writes the same to the json file 
+		f.write(json.dumps(team_standings,indent=4, sort_keys=False))
+		f.close()
+
+
+
+		
+
+		 
 
 
 if __name__ == "__main__":
-    prem = Premier_league()
-    prem.get_fixtures(274)
+	prem = PremierLeague()
+
+	prem.get_clubs()
 
 
 
-    
-    
-            
-    
+
+
+	
+	
+			
+	
 
 
 
