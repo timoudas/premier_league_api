5a231186832acf19630e1a5ed96cab03c5e38333
je223gs <je223gs@student.lnu.se>
2020-02-16 22:41:17 +0100

    Adding some simple html structure and styles

--- API_scraper/view/server.py
+++ API_scraper/view/server.py
@@ -1,10 +1,20 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
-class Serv(BaseHTTPRequestHandler) :
 
+class Serv(BaseHTTPRequestHandler):
+
+    # This class handles all the get requests made to the page.
     def do_GET(self):
-        if self.path == '/':
+        if self.path == '/':  # Home-page
             self.path = '/pages/home.html'
+        try:
+            file_to_open = open(self.path[1:]).read()
+            self.send_response(200)
+        except:
+            file_to_open = "File not found"
+            self.send_response(404)
+        if self.path == '/about':  # About-page
+            self.path = '/pages/about.html'
         try:
             file_to_open = open(self.path[1:]).read()
             self.send_response(200)
@@ -14,7 +24,8 @@ class Serv(BaseHTTPRequestHandler) :
         self.end_headers()
         self.wfile.write(bytes(file_to_open, 'utf-8'))
 
-httpd = HTTPServer(('localhost', 8080), Serv)
+
+httpd = HTTPServer(('localhost', 8080), Serv) # The server is running on localhost:8080.
+print('The server is up and running!')
 
 httpd.serve_forever()
-
