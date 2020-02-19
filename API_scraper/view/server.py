ed76a4749899d3e1e46a5ad897eada8a914dd1dc
je223gs <je223gs@student.lnu.se>
2020-02-19 02:06:10 +0100

    Implementing Flask and setting up env.

--- API_scraper/view/server.py
+++ API_scraper/view/server.py
@@ -1,31 +1,38 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
+from flask import Flask, render_template, url_for, request, redirect
+from datetime import datetime
+
+app = Flask(__name__, template_folder="templates")
+
+""" HOME PAGE """
+@app.route('/')
+def index():
+    return render_template('home/index.html')
+
+""" LEAGUES """
+@app.route('/leagues/viewLeagues')
+def viewLeague():
+    print("Here we load all leagues from db")
+    return render_template('leagues/viewLeagues.html')
+
+""" GAMES """
+@app.route('/games/viewGames')
+def viewGames():
+    print("Here we load all games from db")
+    return render_template('games/viewGames.html')
+
+""" BETS """
+@app.route('/bets/viewBets')
+def viewBets():
+    print("Here we load all bets from db")
+    return render_template('bets/viewBets.html')
+
+""" STATS """
+@app.route('/stats/players')
+def viewPlayerStats():
+    print("Here we load all player stats")
+    return render_template('stats/players.html')
 
 
-class Serv(BaseHTTPRequestHandler):
-
-    # This class handles all the get requests made to the page.
-    def do_GET(self):
-        if self.path == '/':  # Home-page
-            self.path = '/pages/home.html'
-        try:
-            file_to_open = open(self.path[1:]).read()
-            self.send_response(200)
-        except:
-            file_to_open = "File not found"
-            self.send_response(404)
-        if self.path == '/about':  # About-page
-            self.path = '/pages/about.html'
-        try:
-            file_to_open = open(self.path[1:]).read()
-            self.send_response(200)
-        except:
-            file_to_open = "File not found"
-            self.send_response(404)
-        self.end_headers()
-        self.wfile.write(bytes(file_to_open, 'utf-8'))
-
-
-httpd = HTTPServer(('localhost', 8080), Serv) # The server is running on localhost:8080.
-print('The server is up and running!')
-
-httpd.serve_forever()
+if __name__ == "__main__":
+    app.run(debug=True)
+    
