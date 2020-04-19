a3be6dd3ba676b40fdf09fcb677e291d0b7ecbbe
Andreas Timoudas <andreas@Andreass-Air.lan>
2020-04-19 20:54:43 +0200

    fixat data rensning för matcher och spelare

--- API_scraper/model/update_fixturestats.py
+++ API_scraper/model/update_fixturestats.py
@@ -3,7 +3,8 @@ Updates .json files with new data from the api if the json exists
 
 Looks if fixtureID exists in .json, if it doesn't exist it gets append
 """
-from directory import Directory 
+from directory import Directory
+from directory import StorageConfig 
 import pandas as pd
 from pprint import pprint
 from functools import reduce 
@@ -26,14 +27,11 @@ def load_fixture_stats(year):
     """
     try: 
         file = f'EN_PR_{year}_fixturestats.json'
-        stats_file = dirs.load_json(file, '..', 'json', 'params', 'stats')
+        stats_file = dirs.load_json(file, StorageConfig.STATS_DIR)
         stats_file.append({'season': year})
         return stats_file
     except FileNotFoundError as e:
         print(file, "not found")
-
-def read_fixturestats(data):
-    pass
 
 def read_fixtureinfo(data):
     info_all = []
@@ -42,23 +40,81 @@ def read_fixtureinfo(data):
             stats_temp = {}
             if 'info' in d:
                 stats = d['info']
-                stats_temp['age'] = stats.get('age')
-                stats_temp['id'] = stats.get('id')
-                stats_temp['birth'] = deep_get(stats, 'birth.date.label')
-                stats_temp['birth_exact'] = deep_get(stats, 'birth.date.millis')
-                stats_temp['country'] = deep_get(stats, 'birth.country.country')
-                stats_temp['isoCode'] = deep_get(stats, 'birth.country.isoCode')
-                stats_temp['loan'] = deep_get(stats, 'info.loan')
-                stats_temp['position'] = deep_get(stats, 'info.position')
-                stats_temp['positionInfo'] = deep_get(stats, 'info.positionInfo')
-                stats_temp['shirtNum'] = deep_get(stats, 'info.shirtNum')
-                stats_temp['name'] = deep_get(stats, 'name.display')
-                stats_temp['first'] = deep_get(stats, 'name.first')
-                stats_temp['last'] = deep_get(stats, 'name.last')
-                stats_temp['nationalTeam'] = deep_get(stats, 'nationalTeam.country')
-                stats_temp['playerId'] = stats.get('playerId')
-                stats_temp['season'] = data[-1]['season']
+                stats_temp = \
+                    {'gameweek_id' : stats.get('gameweek.id'),
+                    'season_label' : deep_get(stats, 'gameweek.compSeason.label'),
+                    'id' : stats.get('id'),
+
+                    'competition' : deep_get(stats, 'gameweek.compSeason.competition.description'),
+                    'competition_abbr' : deep_get(stats, 'gameweek.compSeason.competition.abbreviation'),
+                    'competition_id' : deep_get(stats, 'gameweek.compSeason.competition.id'),
+
+                    'gameweek' : deep_get(stats, 'gameweek.gameweek'),
+                    'kickoff' : deep_get(stats, 'kickoff.label'),
+                    'kickoff_millis' : deep_get(stats, 'kickoff.millis'),
+
+                    'home_team' : stats['teams'][0]['team']['name'],
+                    'home_team_id' : stats['teams'][0]['team']['club']['id'],
+                    'home_team_shortName' : stats['teams'][0]['team']['shortName'],
+                    'home_team_score' : stats['teams'][0]['score'],
+
+                    'away_team' : stats['teams'][1]['team']['name'],
+                    'away_team_id' : stats['teams'][1]['team']['club']['id'],
+                    'away_team_shortName' : stats['teams'][1]['team']['shortName'],
+                    'away_team_score' : stats['teams'][1]['score'],
+
+                    'ground' : deep_get(stats, 'ground.name'),
+                    'grounds_id' : deep_get(stats, 'ground.id'),
+
+                    'city' : deep_get(stats, 'ground.city'),
+                    'fixtureType' : stats.get('fixtureType'),
+                    'extraTime' : stats.get('extraTime'),
+                    'shootout' : stats.get('shootout'),
+                    'fixture_id' : stats.get('id'),
+
+                    'clock_label' : deep_get(stats, 'clock.label'),
+                    'clock_secs' : deep_get(stats, 'clock.secs'),}
                 info_all.append(stats_temp)
     except TypeError as e:
         print("Check that data exists and is loaded correctly")
     return info_all
+
+def read_fixturestats(data):
+    """In key "stats" followed by teamID followed by key "M"
+
+    """
+    try:
+        stats_all = []
+        for d in data:
+            stats_home = {}
+            stats_away = {}
+            if 'stats' in d:
+                stats = d['stats']
+                info = d['info']
+
+                home_id_key = str(info['teams'][0]['team']['club']['id'])
+                away_id_key = str(info['teams'][0]['team']['club']['id'])
+                stats_home['id'] = info.get('id')
+                stats_away['id'] = info.get('id')
+
+                if home_id_key in stats:
+                    home_stats = stats[home_id_key]['M']
+                    for stats in home_stats:
+                        if stats.get('name') != None:
+                            stats_home['home_' + stats.get('name')] = stats.get('value')
+
+                if away_id_key in stats:
+                    away_stats = stats[away_id_key]['M']
+                    for stats in away_stats:
+                        if stats.get('name') != None:
+
+                            stats_away['away_' + stats.get('name')] = stats.get('value')
+                stats_all.append(stats_home)
+                stats_all.append(stats_away)
+        return stats_all
+    except TypeError as e:
+        print("Check that data exists and is loaded correctly")
+
+if __name__ == '__main__':
+    pprint(read_fixturestats(load_fixture_stats(2019)))
+  
--- API_scraper/model/update_playerstats.py
+++ API_scraper/model/update_playerstats.py
@@ -4,7 +4,7 @@ Updates .json files with new data from the api if the json exists
 Looks if fixtureID exists in .json, if it doesn't exist it gets append
 """
 from directory import Directory 
-from directory import Storageconfig 
+from directory import StorageConfig 
 import pandas as pd
 from pprint import pprint
 from functools import reduce 
@@ -28,7 +28,7 @@ def load_player_stats(year):
     """
     try: 
         file = f'EN_PR_{year}_playerstats.json'
-        stats_file = dirs.load_json(file, Storageconfig.STATS_DIR)
+        stats_file = dirs.load_json(file, StorageConfig.STATS_DIR)
         stats_file.append({'season': year})
         return stats_file
     except FileNotFoundError as e:
@@ -63,24 +63,25 @@ def read_playerinfo(data):
     for d in data:
         stats_temp = {}
         if 'info' in d:
-            stats = d['info'] #Gets the info key in each dict
-            stats_temp['age'] = stats.get('age')
-            stats_temp['id'] = stats.get('id')
-            stats_temp['birth'] = deep_get(stats, 'birth.date.label')
-            stats_temp['birth_exact'] = deep_get(stats, 'birth.date.millis')
-            stats_temp['country'] = deep_get(stats, 'birth.country.country')
-            stats_temp['isoCode'] = deep_get(stats, 'birth.country.isoCode')
-            stats_temp['loan'] = deep_get(stats, 'info.loan')
-            stats_temp['position'] = deep_get(stats, 'info.position')
-            stats_temp['positionInfo'] = deep_get(stats, 'info.positionInfo')
-            stats_temp['shirtNum'] = deep_get(stats, 'info.shirtNum')
-            stats_temp['name'] = deep_get(stats, 'name.display')
-            stats_temp['first'] = deep_get(stats, 'name.first')
-            stats_temp['last'] = deep_get(stats, 'name.last')
-            stats_temp['nationalTeam'] = deep_get(stats, 'nationalTeam.country')
-            stats_temp['playerId'] = stats.get('playerId')
-        stats_temp['season'] = data[-1]['season']
-        info_all.append(stats_temp)
+            stats = d['info']
+            stats_temp = \
+                {'age' : stats.get('age'),
+                'id' : stats.get('id'),
+                'birth' : deep_get(stats, 'birth.date.label'),
+                'birth_exact' : deep_get(stats, 'birth.date.millis'),
+                'country' : deep_get(stats, 'birth.country.country'),
+                'isoCode' : deep_get(stats, 'birth.country.isoCode'),
+                'loan' : deep_get(stats, 'info.loan'),
+                'position' : deep_get(stats, 'info.position'),
+                'positionInfo' : deep_get(stats, 'info.positionInfo'),
+                'shirtNum' : deep_get(stats, 'info.shirtNum'),
+                'name' : deep_get(stats, 'name.display'),
+                'first' : deep_get(stats, 'name.first'),
+                'last' : deep_get(stats, 'name.last'),
+                'nationalTeam' : deep_get(stats, 'nationalTeam.country'),
+                'playerId' : stats.get('playerId'),
+                'season' : data[-1]['season']}
+            info_all.append(stats_temp)
 
     # except TypeError as e:
     #     print("Check that data exists and is loaded correctly")
@@ -107,6 +108,7 @@ def playerstats(start_year, end_year=None):
 
 
 if __name__ == '__main__':
+
     pprint(read_playerinfo(load_player_stats(2018)))
 
 
