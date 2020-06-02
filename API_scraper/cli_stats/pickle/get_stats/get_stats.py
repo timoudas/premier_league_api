cde0ccec7aeea94b62e42b7fe7a4d926cc47b65f
Andreas Timoudas <andreas@Andreass-Air.lan>
2020-06-02 22:09:48 +0200

    removed unused code

--- API_scraper/cli_stats/pickle/get_stats/get_stats.py
+++ API_scraper/cli_stats/pickle/get_stats/get_stats.py
@@ -26,48 +26,6 @@ from tqdm import tqdm
             // Params : compSeasonIds, sort, fixtures, comps, sys
             'player' : '/stats/player/{id}',"""
 
-def load_raw_data(url):
-    """Makes requests against the API
-        Args:
-            url(str): The url that is to be requested
-
-        Returns the json data from the url
-    """
-    page = 0
-    data_temp = [] #Placeholder when url has multiple pages
-    #Loops all the pages on the requested url
-    while True:
-        headers = {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-                            'Origin': 'https://www.premierleague.com',
-                            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'
-                  }
-        params = (('pageSize', '100'),
-                ('page', str(page),))
-
-        #try to obtain the json-data from the url
-        try:
-            response = requests.get(url, params = params, headers=headers).json()  
-            if url.endswith('staff'):
-                data = response['players']
-                return data
-            elif 'fixtures' in url:
-                data = response["content"]
-                #loop to get info for each game
-                data_temp.extend(data)
-            else:
-                data = response['content']
-                # note: bit of a hack, for some reason 'id' is a float, but everywhere it's referenced, it's an int
-                for d in data:
-                    d['id'] = int(d['id'])
-                return data
-        except Exception as e:
-            print(e, 'Something went wrong with the request')
-            return {}
-
-        page += 1
-        if page == response["pageInfo"]["numPages"]:
-            break
-
     for d in data_temp:
         d['id'] = int(d['id'])
     return data_temp
