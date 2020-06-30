3e084ba627e4ab3a95f6fcb3e6b7171dd7e3b1aa
repl.it user <replituser@example.com>
2020-06-30 13:17:41 +0000

    added functions to download all images and save them as the players last name

--- cli_stats/get_data/api_scraper/get_player_img.py
+++ cli_stats/get_data/api_scraper/get_player_img.py
@@ -1,7 +1,9 @@
 from api_scraper import Football
 import requests
 from bs4 import BeautifulSoup
+import shutil
 
+IMG_SAVE_PATH = '../../../dashboard/assets/'
 
 fb = Football()
 
@@ -18,8 +20,44 @@ def get_team_ids():
 
 def get_img_elem(team_request):
   page = team_request.text
-  soup = BeautifulSoup(page)
-  tags = soup.findAll('img', {'class': 'img statCardImg'})
-  print(tags)
+  soup = BeautifulSoup(page, 'html.parser')
+  tags_raw = soup.find_all('img', {'class': 'img statCardImg'})
+  tags = [tag.get('src') for tag in tags_raw]
+  return tags
 
-get_img_elem(get_team_url(1))
+def get_player_name(team_request):
+  page = team_request.text
+  soup = BeautifulSoup(page, 'html.parser')
+  tags_raw = soup.find_all('h4', {'class': 'name'})
+  tags = [tag.text for tag in tags_raw]
+  return tags
+
+def create_dict(team_request):
+  images = get_img_elem(team_request)
+  player_names = get_player_name(team_request)
+  player_url_tuple = zip(player_names, images)
+  player_img_dict = {name: url for name, url in player_url_tuple}
+  return player_img_dict
+
+
+
+def all_player_imgs():
+  team_ids = get_team_ids()
+  results = []
+  for team_id in team_ids:
+    team_players = get_team_url(team_id)
+    player_dict = create_dict(team_players)
+    results.append(player_dict)
+  return results
+
+def download_images():
+  all_players = all_player_imgs()
+  for d in all_player_imgs:
+    for key, value in d.items():
+      response = requests.get(value, stream=True)
+      last_name = key.split(' ')[-1]
+      with open(IMG_SAVE_PATH + last_name + '.png', 'wb') as out_file:
+        shutil.copyfileobj(response.raw, out_file)
+      del response
+
+print(all_player_imgs())
