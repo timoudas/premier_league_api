3f16694ce0e50b850490384d8e8a0c69b247cd03
Your Name <you@example.com>
2020-12-27 18:29:56 +0100

    refactored getstatsmethods

--- cli_stats/get_data/static_types.py
+++
@@ -1,5 +0,0 @@
-FILE_NAMES = {'f_stats': 'fixturestats.json',
-	          'p_stats': 'playerstats.json',
-	          't_squad': 'teamsquads.json',
-	          't_stand': 'teamstandings.json',
-	          'l_stats': 'league_standings.json'}
